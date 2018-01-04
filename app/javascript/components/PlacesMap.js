de463ec14ce8e37bdad0e2cd66205c5085fb86c1
Mike Tamis <tamis.mike@gmail.com>
2018-01-05 12:00:23 +1300

    Added places maker to map

--- app/javascript/components/PlacesMap.js
+++ app/javascript/components/PlacesMap.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import ReactMapboxGl from 'react-mapbox-gl'
+import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl'
 import { connect } from 'react-redux'
+import {  Icon  } from 'semantic-ui-react'
 
 const MapBoxAccessToken = 'pk.eyJ1IjoibWlrZXRhbWlzIiwiYSI6ImNqYzA5N20zdjA2eWYycW1wcjZldXFzbm8ifQ.Vbx9_kz5j0ZxIPghNDRtkQ';
 
+const defaultCenter = [0, 0];
 
 const Map = ReactMapboxGl({
   accessToken: MapBoxAccessToken,
@@ -14,7 +16,14 @@ const PlacesMap = ({ places }) => (
     style="mapbox://styles/mapbox/outdoors-v9" // eslint-disable-line
     containerStyle={{ height: '100%', width: '100%' }}
     onStyleLoad={ (map) => { map.resize() } }
+    center={ places.length ? [places[0].lng, places[0].lat] : defaultCenter }
+    zoom={[5]}
   >
+    {places.map(place =>
+      <Marker anchor="bottom" offset={-10} coordinates={[place.lng, place.lat]}>
+        <Icon name='marker'  style={{ fontSize: '3em' }}/>
+      </Marker>
+    )}
   </Map>
 )
 
