50d141f9c9909301a3ff086101f012d7b8282845
Mike Tamis <tamis.mike@gmail.com>
2018-01-04 20:53:24 +1300

    Added Redux Devtools

--- app/javascript/components/Root.js
+++ app/javascript/components/Root.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
@@ -16,9 +16,11 @@ if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger())
 }
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
-  applyMiddleware(...middleware)
+  composeEnhancers(applyMiddleware(...middleware))
 )
 
 export default () => <Provider store={store}>
